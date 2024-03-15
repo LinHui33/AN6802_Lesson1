3846cdd18c93f3b66def9980d258d18cfc52ef0f
LIN HUI <97448457+LinHui33@users.noreply.github.com>
2024-03-15 20:37:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,43 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 
+name_flag = False
+name = ""
+
 @app.route("/",methods = ["GET","POST"])
 def index():
     return render_template("index.html")
 
 @app.route("/main",methods = ["GET","POST"])
 def main():
-    name = request.form.get("name")
+    global name_flag, name
+    if name_flag == False:
+        name = request.form.get("name")
+        name_flag = True
+        
+        conn = sqlite3.connect("log.db")
+        c = conn.cursor()
+        current_dateTime = datetime.datetime.now()
+        c.execute("INSERT INTO employee (name, timestamp) VALUES (?, ?)", (name, current_dateTime))
+        conn.commit()
+        c.close()
+        conn.close()
+    
     return render_template("main.html", name = name)
 @app.route("/ethical_test",methods = ["GET","POST"])
 def ethical_test():
     return render_template("ethical_test.html")
+
+@app.route("/query",methods = ["GET","POST"])
+def query():
+    conn = sqlite3.connect("log.db")
+    results = conn.execute("select * from employee")
+    output = ""
+    for row in results:
+        output = output + str(row) + "<br>"
+    output = Markup(output)
+    conn.close()
+    return render_template("query.html", text = output)
 
 @app.route("/answer",methods = ["GET","POST"])
 def answer():
@@ -28,6 +54,9 @@ def answer():
 
 @app.route("/end",methods = ["GET","POST"])
 def end():
+    global name_flag, name
+    name_flag = False
+    name = ""
     return render_template("end.html")
     
     
