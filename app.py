757f193a75f201693f2f2733c87414edfdf46c34
LIN HUI <97448457+LinHui33@users.noreply.github.com>
2024-03-22 19:51:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 from flask import Flask,request,render_template,Markup
 import datetime
 import sqlite3
 
+
+# In[2]:
+
+
 app = Flask(__name__)
+
+
+# In[ ]:
 
 
 name_flag = False
@@ -35,6 +45,14 @@ def main():
 def ethical_test():
     return render_template("ethical_test.html")
 
+@app.route("/answer",methods = ["GET","POST"])
+def answer():
+    answer = request.form.get("options")
+    if answer.lower() == "true":
+        return render_template("wrong.html")
+    else:
+        return render_template("correct.html")
+
 @app.route("/query",methods = ["GET","POST"])
 def query():
     conn = sqlite3.connect("log.db")
@@ -45,14 +63,25 @@ def query():
     output = Markup(output)
     conn.close()
     return render_template("query.html", text = output)
+@app.route("/foodexp", methods =  ["GET","POST"])
+def foodexp():
+    return render_template("foodexp.html")
 
-@app.route("/answer",methods = ["GET","POST"])
-def answer():
-    answer = request.form.get("options")
-    if answer.lower() == "true":
-        return render_template("wrong.html")
-    else:
-        return render_template("correct.html")
+@app.route("/prediction", methods =  ["GET","POST"])
+def prediction():
+    income = float(request.form.get("income"))
+    expense = income *0.485 + 147.47
+    return render_template("prediction.html", expense = expense)
+
+@app.route("/delete",methods = ["GET","POST"])
+def deleteDB():
+    conn = sqlite3.connect("log.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM employee;")
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template("deleteDB.html")
 
 @app.route("/end",methods = ["GET","POST"])
 def end():
@@ -65,6 +94,9 @@ def end():
 
 if __name__ == "__main__":
     app.run()
+
+
+# In[ ]:
 
 
 
